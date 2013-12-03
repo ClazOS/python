a34e241af7bcb4f793742a53b53fd550ba32aa80
Philippe Lemaire <lemaire.philippe@gmail.com>
2013-12-03 11:50:47 +0100

    added function to compute score on a list of cards and fonction to pick or draw

--- games/coloretto.py
+++ games/coloretto.py
@@ -1,10 +1,11 @@
 '''Card Game Coloretto'''
 import random
-# Structure des données
-# Objets : le deck : une liste ? 9 cards par colors, 6 colors
-# Les cards récupérées par les joueurs : listes ?
-# les slots sur lesquels il y a des cards : listes ?
-# La table : une liste des listes qui représentent les slots
+# Data structure
+# Objets : the deck : a list, 9 cards per colors, 6 colors, plus 10 "+2" cards and 3~jokers, and a last turn card.
+# Cards gathered by players = lists
+# Slots where players put cards they draw = a list of the slots. Each slot contains between 0 and 3 cards.
+# Score table = a dictionary of scores given by a number of colored cards.
+
 num_players = 3
 deck = []
 cards_player = []
@@ -17,13 +18,7 @@ color3 = "Yellow"
 color4 = "Blue"
 color5 = "Gray"
 color6 = "Black"
-# Fonctions nécessaires
-# Dessiner le plateau
-# Jouer un tour
-# Prendre un paquet
-# Piocher une carte
-# IA
-# Compter les scores
+score_table = {0: 0, 1: 1, 2: 3, 3: 6, 4: 10, 5: 15, 6: 21}
 
 def deck_regen(deck):
     '''Takes a deck and restores it to the initial state to begin a new game. Returns the new deck.'''
@@ -64,13 +59,30 @@ def game_start(deck, cards_player, cards_CPU1, cards_CPU2):
     cards_CPU1.append('Yellow')
     return deck, cards_player, cards_CPU1, cards_CPU2
 
-def print_board(slots, deck):
-    '''Prints the state of the game: slots with cards in them, and the number of remaining cards'''
+def convert_dict(cards):
+    '''Takes in a list of cards and converts it in a dictionary with key = "type of card" and value = number of cards of that type. Returns the dictionary created. Will be used to display player's cards.'''
+    d = {}
+    d[color1] = cards.count(color1)
+    d[color2] = cards.count(color2)
+    d[color3] = cards.count(color3)
+    d[color4] = cards.count(color4)
+    d[color5] = cards.count(color5)
+    d[color6] = cards.count(color6)
+    d["Jocker"] = cards.count("Joker")
+    d["+2"] = cards.count("+2")
+    return d
+
+def print_board(slots, deck, cards_player, cards_CPU1, cards_CPU2):
+    '''Prints the state of the game: slots with cards in them, cards the players have gathered and the number of remaining cards'''
     print("Remaining cards in deck: ",str(len(deck)))
+    print("You have: ", convert_dict(cards_player))
+    print("CPU1 has: ", convert_dict(cards_CPU1))
+    print("CPU1 has: ", convert_dict(cards_CPU2))
     for i in range(0,len(slots)):
         print("Slot ",str(i + 1), " contains:","\n")
         for card in slots[i]:
             print(card, "\n")
+
 
 def draw_card(deck, slots):
     '''Lets human player draw a card, look at it, and select a slot to put it'''
@@ -89,7 +101,7 @@ def draw_card(deck, slots):
         card = deck.pop(0)
         print("The card you drew is ",card)
         print("Where do you want to put it?")
-        print_board(slots, deck)
+        print_board(slots, deck, cards_player, cards_CPU1, cards_CPU2)
         choice = ''
         choices = []
         # populates choices list with valid choices as strings
@@ -122,27 +134,74 @@ def pick_slot(slots, cards_player):
     del slots[choice]
     return slots, cards_player
 
-def convert_dict(cards):
-    '''Takes in a list of cards and converts it in a dictionary with key = "type of card" and value = number of cards of that type. Returns the dictionary created. Will be used to display player's cards and compute the score(maybe).'''
-    d = {}
-    d[color1] = cards.count(color1)
-    d[color2] = cards.count(color2)
-    d[color3] = cards.count(color3)
-    d[color4] = cards.count(color4)
-    d[color5] = cards.count(color5)
-    d[color6] = cards.count(color6)
-    d["Jocker"] = cards.count("Joker")
-    d["+2"] = cards.count("+2")
-    return d
+def compute_score(cards):
+    '''Takes a list of cards as argument. Computes the score for these cards. Assigns Jocker cards to a color in order to maximize score, and add the value of the +2 cards.
+    Returns score as an integer.'''
+    score = 0
+    #count_list will be a list of the number of colored cards hold. The three biggest numbers will add to the score, the other numbers will reduce the score according to the same table.
+    count_list = []
+    count_list.append(cards.count(color1))
+    count_list.append(cards.count(color2))
+    count_list.append(cards.count(color3))
+    count_list.append(cards.count(color4))
+    count_list.append(cards.count(color5))
+    count_list.append(cards.count(color6))
+    #order list by decreasing order
+    count_list.sort(reverse=True)
+    #jokers will hold the number of jokers to assign to a color. Having more than 6 cards of a color is not beneficial, so we check if there is a 5 in count_list, and if there is, add 1 to it and remove a joker. Else, we look for a 4, and so on. Fortunately, the count_list is already sorted as needed, so we only need to check its items starting from the first one.
+    jokers = cards.count("Joker")
+    while jokers > 0:
+        for i in range(0, len(count_list)):
+            if count_list[i] < 6:
+                count_list[i] += 1
+                jokers -= 1
+                break
+    # Now that the jokers are assigned to the best color possible, let's compute score from colored cards.
+    for num in count_list[:3]:
+        if num > 6:
+            score += 21
+        else:
+            score += score_table[num]
+    for num in count_list[3:]:
+        if num > 6:
+            score -= 21
+        else:
+            score -= score_table[num]
+
+    # Add the value of all the "+2" cards.
+    score += cards.count("+2") * 2
+    return score
+
+def ia_pick_or_draw(slots,cards):
+    '''Used to chose picking a slot of cards, or drawing, for CPU. Returns "I pick." or "I draw.".'''
+    # First check if all slots are either full or empty, and return the only legal move.
+    all_slots_empty = True
+    all_slots_full = True
+    for i in range(0,len(slots)):
+        if slots[i] != []:
+            all_slots_empty = False
+        if len(slots[i]) < 3:
+            all_slots_full = False
+    if all_slots_empty:
+        return "I draw."
+    if all_slots_full:
+        return "I pick."
+    # Let's use a threshold value to decide to pick. If a slot contains cards that would add threshold or more to my score, then decide to pick.
+    threshold = 6
+    for slot in slots:
+        if compute_score(cards + slot) - compute_score(cards) >= threshold:
+            return "I pick."
+    return "I draw."
 
 
 # Debug zone
-deck, cards_player, cards_CPU1, cards_CPU2 = game_start(deck, cards_player, cards_CPU1, cards_CPU2)
-print(deck)
-slots = new_turn(num_players, slots)
-print_board(slots,deck)
-deck, slots = draw_card(deck, slots)
-print(slots)
-slots, cards_player = pick_slot(slots,cards_player)
-cards_player_dict = convert_dict(cards_player)
-print (cards_player_dict)
+#deck, cards_player, cards_CPU1, cards_CPU2 = game_start(deck, cards_player, cards_CPU1, cards_CPU2)
+#print(deck)
+#slots = new_turn(num_players, slots)
+#print_board(slots, deck, cards_player, cards_CPU1, cards_CPU2)
+#deck, slots = draw_card(deck, slots)
+#print(slots)
+#slots, cards_player = pick_slot(slots,cards_player)
+#print(compute_score(cards_player))
+#print(compute_score(["Green","Green","Red","Red","Red","Red","Red","Red","Red","Green","Green","Green","Green","Joker","+2"]))
+print(ia_pick_or_draw([[1,2,3],["Green","Green","Red"],[1,2,3]],["Green"]))
